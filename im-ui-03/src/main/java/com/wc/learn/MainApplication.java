c60989e674138bf9ffcaf5af2e4e370133010ede
wangC1998 <13520215149@163.com>
2023-05-14 10:20:52 +0800

    王超, feat: 窗体代码搭建完成,进行代码提交

---
+++ im-ui-03/src/main/java/com/wc/learn/MainApplication.java
@@ -0,0 +1,26 @@
+package com.wc.learn;
+
+import com.wc.learn.view.Login;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author wc
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Login login = new Login();
+        login.show();
+        login.move();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
