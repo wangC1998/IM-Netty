09374ec9a1e19f9f720d26dbc2f21aa9a7a280c8
wangC1998 <13520215149@163.com>
2023-05-14 16:33:34 +0800

    王超, feat: 登录窗口的重构操作

--- im-ui-03-refactor/src/main/java/com/wc/learn/MainApplication.java
+++ im-ui-03-refactor/src/main/java/com/wc/learn/MainApplication.java
@@ -1,13 +1,8 @@
 package com.wc.learn;
 
-import com.wc.learn.view.Login;
-import com.wc.learn.view.login.ILoginEvent;
 import com.wc.learn.view.login.ILoginMethod;
 import com.wc.learn.view.login.LoginController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -18,13 +13,7 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        ILoginMethod login = new LoginController(new ILoginEvent() {
-            @Override
-            public void doLoginCheck(String userId, String userPassword) {
-                System.out.println("登录的userId:" + userId + ", 登录的密码:" + userPassword);
-            }
-        });
-
+        ILoginMethod login = new LoginController((userId, userPassword) -> System.out.println("登录的userId:" + userId + ", 登录的密码:" + userPassword));
         login.doShow();
     }
 
